3379f8915fa98f3f70a8ff3e54d995a95f052ece
magnusjwatson2786 <mearunprabhakar14@gmail.com>
2021-09-15 15:15:09 +0530

    fixing track sync, silence trail

--- tts.py
+++ tts.py
@@ -1,5 +1,6 @@
-import parsesrt,os,pyttsx3,time
-from pydub import AudioSegment as adseg
+import parsesrt,os,pyttsx3, time
+from pydub import AudioSegment as adseg, silence
+from pydub import effects
 
 srtpath="test1.srt"
 sppath="speech1"
@@ -22,7 +23,6 @@ def getTime(st:str,idx:int)->int:
     ss=int(a.split(":")[2].replace(",",""))
     # print(hh,mm,ss)
     totalmillisecs=hh*60*60*1000+mm*60*1000+ss
-    # print(totalsecs)
     return totalmillisecs
 
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
@@ -33,6 +33,60 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     if iteration == total: 
         print()
 
+def speed_change(sound, speed=1.0):
+    sound_with_altered_frame_rate = sound._spawn(sound.raw_data, overrides={
+         "frame_rate": int(sound.frame_rate * speed)
+      })
+    return sound_with_altered_frame_rate.set_frame_rate(sound.frame_rate)
+
+def compile():
+    nl=len(parsesrt.lns.keys())
+    print("Compilation started") 
+    holder=adseg.empty()
+    lastime=0
+    printProgressBar(0, 100, prefix = 'Compiling:', suffix = 'Complete', length = 50)
+    newtime=getTime(parsesrt.tsmp[1],0)
+    slc=adseg.silent(newtime)
+    # lastime=getTime(parsesrt.tsmp[i+1],1)
+    holder=holder.append(slc, crossfade=0)
+    for i in range(1,nl+1):
+        crsfade=0
+        # lastime=getTime(parsesrt.tsmp[i+1],1)
+        startime=getTime(parsesrt.tsmp[i],0)
+        endtime=getTime(parsesrt.tsmp[i],1)
+        sublength=endtime-startime
+        if not i+1>nl:
+            nextime=getTime(parsesrt.tsmp[i+1],0)
+        if i==nl:
+            nextime=endtime
+        midlength=nextime-endtime
+        # compensate=midlength*0.25
+        part=remove_trailing_silence(adseg.from_file(os.path.join(sppath,str(i)+".wav")))
+        # if (len(part) + len(holder))>endtime:
+        if len(part) > sublength:
+            new_speed=len(part)/(sublength)
+            part=part.speedup(new_speed,150,0)
+        
+        holder=holder.append(part, crossfade=crsfade)
+        diff = nextime-len(holder)
+        # blanktrack=nextime-endtime
+        slc=adseg.silent(diff)
+        holder=holder.append(slc, crossfade=crsfade)
+        # compensate=(lastime-newtime)-len(part)
+        # lastime-=compensate
+        printProgressBar(i, nl, prefix = 'Compiling:', suffix = 'Complete', length = 50)
+
+    holder.export("full2.wav", format="wav")
+    print("compilation finished")
+# # for key in parsesrt.tsmp.keys():
+# #     part=adseg.from_file(os.path.join(sppath,str(key)+".wav"))
+# #     holder=holder.append(part)
+
+def remove_trailing_silence(sound):
+    endtrim=silence.detect_leading_silence(sound.reverse())
+    return sound[:len(sound)-endtrim]
+
+
 print("TTS started")
 start_time = time.time()
 nl=len(parsesrt.lns.keys())
@@ -40,38 +94,15 @@ nl=len(parsesrt.lns.keys())
 printProgressBar(0, 100, prefix = 'TTS:', suffix = 'Converted', length = 50)
 for key in parsesrt.lns.keys():
     # print(key)
-    ttsx(key)
+    ttsx(key) #--SKIP FOR TESTING
     printProgressBar(key, nl, prefix = 'TTS:', suffix = 'Converted', length = 50)
-
 
 print("tts finished")
 
-def compile():
-    print("Compilation started") 
-    holder=adseg.empty()
-    lastime=0
-    printProgressBar(0, 100, prefix = 'Compiling:', suffix = 'Complete', length = 50)
-    for i in range(nl):
-        # print(i+1)
-        newtime=getTime(parsesrt.tsmp[i+1],0)
-        slc=adseg.silent(newtime-lastime)
-        lastime=getTime(parsesrt.tsmp[i+1],1)
-        holder=holder.append(slc, crossfade=0)
-        part=adseg.from_file(os.path.join(sppath,str(i+1)+".wav"))
-        compensate=(lastime-newtime)-len(part)
-        holder=holder.append(part, crossfade=0)
-        lastime-=compensate
-        printProgressBar(i+1, nl, prefix = 'Compiling:', suffix = 'Complete', length = 50)
-
-    holder.export(os.path.join(sppath,"full.wav"), format="wav")
-    print("compilation finished")
-
-print("--- %s seconds ---" % (time.time() - start_time))
+print("TTS converted in %s seconds " % (time.time() - start_time))
 
 start_time = time.time()
 
 compile()
 
-print("--- %s seconds ---" % (time.time() - start_time))
-
-
+print("Compiled in %s seconds " % (time.time() - start_time))
